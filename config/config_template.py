1678fb5851cab2da35645e7ae07af45f78db78fb
clcriswell <codycriswell@yahoo.com>
2025-06-24 22:37:35 -0700

    Update config_template.py

--- config/config_template.py
+++ config/config_template.py
@@ -1,6 +1,6 @@
 
 # Copy this file to config.py and fill in your real OpenAI keys.
 OPENAI_API_KEYS = [
-    "sk-your-first-openai-key",
-    "sk-your-second-openai-key"
+    "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",
+    "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"
 ]
